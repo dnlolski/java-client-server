49398641015fcada3004e79fcf09b004e7e37167
dnlolski <daniel.olszewski00@gmail.com>
2019-03-27 14:56:12 +0100

    fixed some issues with socket closure

--- client/src/com/company/Main.java
+++ client/src/com/company/Main.java
@@ -7,38 +7,36 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-        //get the localhost IP address, if server is running on some other IP, you need to use that
-        InetAddress host = InetAddress.getLocalHost();
-        Socket socket = null;
-        ObjectOutputStream oos = null;
-        ObjectInputStream ois = null;
+
+
+        Socket socket = new Socket("localhost", 1250);
+        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
         int number = 0;
 
         Scanner scanner = new Scanner(System.in);
         while(true){
             //establish socket connection to server
-            socket = new Socket(host.getHostName(), 9876);
             //write to socket using ObjectOutputStream
-            oos = new ObjectOutputStream(socket.getOutputStream());
             System.out.println("Sending request to Socket Server");
             System.out.println("Enter number to square: ");
             number = scanner.nextInt();
             if (number == 0) {
                 oos.writeObject(number);
-                oos.close();
                 break;
             }
             else {
                 oos.writeObject(number);
             }
             //read the server response message
-            ois = new ObjectInputStream(socket.getInputStream());
+
             String numberSquared = (String) ois.readObject();
             System.out.println("Message: " + numberSquared);
             //close resources
-            ois.close();
-            oos.close();
             Thread.sleep(100);
         }
+        ois.close();
+        oos.close();
+
     }
 }
--- server/src/com/company/Main.java
+++ server/src/com/company/Main.java
@@ -7,41 +7,32 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException, ClassNotFoundException{
-        //static ServerSocket variable
-        ServerSocket server;
-        //socket server port on which it will listen
-        int port = 9876;
-        //create the socket server object
-        server = new ServerSocket(port);
-        //keep listens indefinitely until receives 'exit' call or program terminates
+
+        int port = 1250;
+
+        ServerSocket server = new ServerSocket(port);
+        Socket socket = server.accept();
+        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+
         while(true){
             System.out.println("Waiting for the client request");
-            //creating socket and waiting for client connection
-            Socket socket = server.accept();
+
             System.out.println("Client connected \n");
-            //read from socket to ObjectInputStream object
-            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-            //convert ObjectInputStream object to String
             Integer number = (Integer) ois.readObject();
             System.out.println("Number received to square: " + number);
-            //terminate the server if client sends exit request
+            //terminate the server if client sends 0
             if(number.equals(0)){
-                ois.close();
-                socket.close();
                 break;
             }
-            //create ObjectOutputStream object
-            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-            //write object to Socket
+
             oos.writeObject("Square of received number: " + number*number);
-            //close resources
-            ois.close();
-            oos.close();
-            socket.close();
 
         }
         System.out.println("Shutting down Socket server!");
         //close the ServerSocket object
+        ois.close();
+        socket.close();
         server.close();
     }
 }
